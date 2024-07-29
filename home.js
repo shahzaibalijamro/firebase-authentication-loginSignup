4a0ceee528da561e295a82fefd34d64fa8864685
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-07-30 03:16:26 +0500

    commands added

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('#btn');
 onAuthStateChanged(auth, (user) => {
     if (user) {
         const uid = user.uid;
-        console.log(uid);
+        console.log(user);
     } else {
         window.location = 'index.html'
     }
@@ -16,4 +16,35 @@ btn.addEventListener('click', () => {
     }).catch((error) => {
         alert('An error appeared');
     });
+})
+let todo = [];
+let input = document.querySelector('.input');
+let div = document.querySelector('.main');
+let addTodo = document.querySelector('#addTodo');
+let deleteTodo = document.querySelector('#deleteTodo');
+let editTodo = document.querySelector('#editTodo');
+function printTodo() {
+    div.innerHTML = '';
+    for (let i = 0; i < todo.length; i++) {
+        div.innerHTML += `
+        <div class="list">
+            <h2 class="main-head">${todo[i]}</h2>
+            <div class="icons">
+                <div id="editTodo">
+                    <i class="fa-solid fa-1 fa-pen-to-square"></i>
+                </div>
+                <div id="deleteTodo">
+                    <i class="fa-solid fa-2 fa-trash"></i>
+                </div>
+            </div>
+        </div>
+        `
+        ;
+    }
+}
+addTodo.addEventListener('click', ()=>{
+    todo.push(input.value);
+    input.value = '';
+    printTodo();
+    console.log(todo);
 })
