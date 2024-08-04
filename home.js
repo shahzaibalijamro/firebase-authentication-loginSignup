20aa8874e3ea7c41f3ad402d05d1aae10c158b92
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-08-04 18:55:48 +0500

    commands added

--- home.js
+++ home.js
@@ -1,8 +1,31 @@
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { 
+    onAuthStateChanged,
+    signOut
+    } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { auth } from "./firebaseConfig.js";
-import { collection, addDoc, getDocs  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
+import { 
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    deleteDoc,
+    updateDoc,
+    Timestamp
+    } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
 import { db } from "./firebaseConfig.js";
+
+
 const btn = document.querySelector('#btn');
+let todo = [];
+let input = document.querySelector('.input');
+let form = document.querySelector('#form');
+let welcomeText = document.querySelector('#welcome-text');
+let img = document.querySelector('#img');
+let div = document.querySelector('.main');
+
+
+
+// check if the user is logged in or not
 onAuthStateChanged(auth, (user) => {
     if (user) {
         const uid = user.uid;
@@ -17,6 +40,9 @@ onAuthStateChanged(auth, (user) => {
         window.location = 'index.html'
     }
 });
+
+
+// sign out button
 btn.addEventListener('click', () => {
     signOut(auth).then(() => {
         console.log('Signed Out!')
@@ -25,23 +51,21 @@ btn.addEventListener('click', () => {
         alert('An error appeared');
     });
 })
+
+
+// getting data from firestore
 async function getData() {
     const querySnapshot = await getDocs(collection(db, "todos"));
     querySnapshot.forEach((doc) => {
-        todo.push(doc.data());
-});
+        todo.push({...doc.data(), id: doc.id});});
     printTodo();
+    console.log(todo[0].time.toDate());
 }
 getData();
-let todo = [];
-let input = document.querySelector('.input');
-let form = document.querySelector('#form');
-let welcomeText = document.querySelector('#welcome-text');
-let img = document.querySelector('#img');
-let div = document.querySelector('.main');
-let addTodo = document.querySelector('#addTodo');
-let deleteTodo = document.querySelector('#deleteTodo');
-let editTodo = document.querySelector('#editTodo');
+
+
+
+// printing data on the screen
 function printTodo() {
     div.innerHTML = '';
     for (let i = 0; i < todo.length; i++) {
@@ -50,30 +74,64 @@ function printTodo() {
             <h2 class="main-head">${todo[i].todo}</h2>
             <div class="icons">
                 <div id="editTodo">
-                    <i class="fa-solid fa-1 fa-pen-to-square"></i>
+                    <i id="updateBtn" class="fa-solid fa-1 fa-pen-to-square"></i>
                 </div>
                 <div id="deleteTodo">
-                    <i class="fa-solid fa-2 fa-trash"></i>
+                    <i id="deleteBtn" class="fa-solid fa-2 fa-trash"></i>
                 </div>
             </div>
         </div>
         `
         ;
     }
+
+
+    // deleting data from both array and firestore
+    let deleteBtn = document.querySelectorAll('#deleteBtn');
+    deleteBtn.forEach((btn,index)=>{
+        btn.addEventListener('click', async()=>{
+            await deleteDoc(doc(db, "todos", todo[index].id));
+            console.log('data deleted');
+            todo.splice(index,1);
+            printTodo();
+        })
+    })
+
+
+
+    // updating data from both array and firestore
+    let updateBtn = document.querySelectorAll('#updateBtn');
+    updateBtn.forEach((btn,index)=>{
+        btn.addEventListener('click', async()=>{
+            const updatedVal = prompt('Enter the updated value!', todo[index].todo)
+            const updateRef = doc(db, "todos", todo[index].id);
+            await updateDoc(updateRef, {
+                todo: `${updatedVal}`
+            });
+            console.log('data updated');
+            todo[index].todo = updatedVal
+            printTodo();
+        })
+    })
 }
+
+
+
+// pushing data to the array and to the firestore
 form.addEventListener('submit', async (event)=>{
     event.preventDefault();
-    todo.push({
-        todo: input.value
-    }
-    );
-    printTodo();
     try {
         const docRef = await addDoc(collection(db, "todos"), {
-            todo: input.value
+            todo: input.value,
+            time: Timestamp.fromDate(new Date())
+        });
+        console.log("Document written with ID: ", docRef.id);
+        todo.push({
+            todo: input.value,
+            id: docRef.id,
         });
         input.value = '';
-        console.log("Document written with ID: ", docRef.id);
+        printTodo();
     } catch (e) {
         console.error("Error adding document: ", e);
     }
